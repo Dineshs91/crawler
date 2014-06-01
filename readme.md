fa254d720086a0f3533dda4fb4ad42188041f450
dinesh <dsdiscoverdinesh@gmail.com>
2014-06-01 17:07:17 +0530

    Initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+Crawler:
+
+Crawler crawls web sites. Start by giving a website address. Depth First Search (DFS) and Breadth First Search (BFS) algorithm is used for exploring. You can choose which algorithm to use. You also have to specify the branching factor.
---
+++ tests/test_TestCrawler.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+import unittest
+from ds.crawlerDFS import Crawler
+
+class TestCrawler(unittest.TestCase):
+    def setUp(self):
+        self.START_URL = 'http://www.facebook.com/'
+        self.BRANCHING_FACTOR = 10
+        self.c = Crawler(self.START_URL, self.BRANCHING_FACTOR)
+
+    def test_checkUrl(self):
+        ans = self.c.checkUrl(self.START_URL, [])
+        #print ans
+        self.assertTrue(len(ans) > 0)
+
+    def test_startDFS(self):
+        ans = self.c.startDFS()
+        #print ans
+        self.assertTrue(len(ans) > 0)
+        #print 'No of links', len(ans)
+        
+    def test_startBFS(self):
+        ans = self.c.startBFS()
+        #print ans
+        self.assertTrue(len(ans) > 0)
+        #print 'No of links', len(ans)       
+        
+if __name__ == "__main__":
+    unittest.main()

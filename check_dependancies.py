cd3759cca794bdeca0fb52ac37653c9b875d5b43
DINESH S <dineshpy07@gmail.com>
2015-07-21 23:17:13 +0530

    Fix indentation

--- check_dependancies.py
+++ check_dependancies.py
@@ -1,8 +1,8 @@
 try:
-	import BeautifulSoup 
-	print 'All requirements are satisfied'
+    import BeautifulSoup 
+    print 'All requirements are satisfied'
 except ImportError:
-	print "Please install BeautifulSoup. Look into readme for instructions"
+    print "Please install BeautifulSoup. Look into readme for instructions"
 
 
 	

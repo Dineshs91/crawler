059e23cb0b227eb753c733c30995a74855a51cf4
DINESH S <dineshpy07@gmail.com>
2015-07-21 23:18:34 +0530

    Remove all commented print statements

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -11,20 +11,15 @@ class TestCrawler(unittest.TestCase):
 
     def test_get_url(self):
         ans = self.c.get_url(self.START_URL, [])
-        #print ans
         self.assertTrue(len(ans) > 0)
 
     def test_start_dfs(self):
         ans = self.c.start_dfs()
-        #print ans
         self.assertTrue(len(ans) > 0)
-        #print 'No of links', len(ans)
         
     def test_start_bfs(self):
         ans = self.c.start_bfs()
-        #print ans
         self.assertTrue(len(ans) > 0)
-        #print 'No of links', len(ans)       
         
 if __name__ == "__main__":
     unittest.main()
